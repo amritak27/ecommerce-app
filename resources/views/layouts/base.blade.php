ac623103e4fd3603d5582af601af835effed9d63
Amruta <amritakulkarni04@gmail.com>
2024-04-05 11:24:43 +0100

    login and register template update

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -61,13 +61,13 @@
         <div class="row">
             <div class="col-xl-3 col-lg-2">
                 <div class="header__logo">
-                    <a href="./index.html"><img src="img/logo.png" alt=""></a>
+                    <a href="{{route('app.index')}}"><img src="img/logo.png" alt=""></a>
                 </div>
             </div>
             <div class="col-xl-6 col-lg-7">
                 <nav class="header__menu">
                     <ul>
-                        <li class="active"><a href="./index.html">Home</a></li>
+                        <li class="active"><a href="{{route('app.index')}}">Home</a></li>
                         <li><a href="#">Women’s</a></li>
                         <li><a href="#">Men’s</a></li>
                         <li><a href="./shop.html">Shop</a></li>
